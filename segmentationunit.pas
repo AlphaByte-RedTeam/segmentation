d2e01804525c13a5245efc762dbf275193896f4e
Janice Claresta Lingga <andrew.avv03@gmail.com>
2022-09-15 12:10:05 +0700

    feat: make upload button functional

--- segmentationunit.pas
+++ segmentationunit.pas
@@ -19,6 +19,7 @@ type
     btnGray: TButton;
     openDialog: TOpenDialog;
     saveDialog: TSaveDialog;
+    procedure btnUploadClick(Sender: TObject);
   private
 
   public
@@ -31,6 +32,27 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btnUploadClick(Sender: TObject);
+var
+  i, j: integer;
+begin
+  if (openDialog.Execute) then
+  begin
+    imgSrc.Picture.LoadFromFile(openDialog.FileName);
+    for j:=0 to imgSrc.Height-1 do
+    begin
+      for i:=0 to imgSrc.Width-1 do
+      begin
+        bmpR[i, j] := getRValue(imgSrc.Canvas.Pixels[i, j]);
+        bmpG[i, j] := getGValue(imgSrc.Canvas.Pixels[i, j]);
+        bmpB[i, j] := getBValue(imgSrc.Canvas.Pixels[i, j]);
+      end;
+    end;
+  end;
+end;
 
 end.
 
