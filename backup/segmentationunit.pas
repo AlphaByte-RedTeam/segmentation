aba119b2ae02d6f28523af7fc6ec53e1ba46f323
FeriAndika-hub <feri14andika@gmail.com>
2022-09-22 13:15:01 +0700

    feat: add dilasi procedure

--- backup/segmentationunit.pas
+++ backup/segmentationunit.pas
@@ -30,6 +30,7 @@ type
     tbBiner: TTrackBar;
     procedure btnBinaryClick(Sender: TObject);
     procedure btnDeteksiTepiClick(Sender: TObject);
+    procedure btnDilasiClick(Sender: TObject);
     procedure btnErosiClick(Sender: TObject);
     procedure btnGrayClick(Sender: TObject);
     procedure btnSaveClick(Sender: TObject);
@@ -188,6 +189,79 @@ begin
   end;
    end;
 
+procedure TForm1.btnDilasiClick(Sender: TObject);
+var
+  i, j, k, l  : integer;
+  temp : boolean;
+  objek, latar : integer;
+begin
+  //objek berwarna hitam
+  objek := 1;
+  latar := 0;
+
+  for j := -1 to 1 do
+  begin
+    for i := -1 to 1 do
+    begin
+      SE[i,j] := 1;
+    end;
+  end;
+
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+          temp := True;
+          for l:=-1 to 1 do
+          begin
+            for k:=-1 to 1 do
+            begin
+              if (SE[k,l]>=0) then
+              begin
+                Temp := Temp AND (bmpBinary[i+k,j+l]=SE[k,l]);
+              end;
+            end;
+          end;
+          if temp then
+            hasilBiner[i,j] := objek
+          else
+            hasilBiner[i,j] := latar;
+          imgMod.Canvas.Pixels[i,j]:= RGB(hasilBiner[i,j]*255, hasilBiner[i,j]*255, hasilBiner[i,j]*255);
+          end;
+    end;
+
+  for j:=0 to imgSrc.Height-1 do
+  begin
+    for i:=0 to imgSrc.Width-1 do
+    begin
+      bmpBinary[i,j] := hasilBiner[i,j];
+      if j=0 then
+      begin
+        bmpR[i,-1] := bmpR[i,j];
+        bmpG[i,-1] := bmpG[i,j];
+        bmpB[i,-1] := bmpB[i,j];
+      end;
+      if j=imgSrc.Height-1 then
+      begin
+        bmpR[i,j+1] := bmpR[i,j];
+        bmpG[i,j+1] := bmpG[i,j];
+        bmpB[i,j+1] := bmpB[i,j];
+      end;
+      if i=0 then
+      begin
+        bmpR[-1,j] := bmpR[i,j];
+        bmpG[-1,j] := bmpG[i,j];
+        bmpB[-1,j] := bmpB[i,j];
+      end;
+      if i=imgSrc.Width-1 then
+      begin
+        bmpR[i+1,j] := bmpR[i,j];
+        bmpG[i+1,j] := bmpG[i,j];
+        bmpB[i+1,j] := bmpB[i,j];
+      end;
+    end;
+  end;
+
 procedure TForm1.btnErosiClick(Sender: TObject);
 var
   i, j, k, l  : integer;
@@ -260,5 +334,5 @@ begin
       end;
     end;
   end;
-
+end;
 end.
